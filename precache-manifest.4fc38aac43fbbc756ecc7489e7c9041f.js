db4d221bc65da816c31da8ed45a35b409ece403e
Daniel Schillat <daniel.schillat@exozet.com>
2020-01-05 14:02:09 +0100

    Updates

---
+++ precache-manifest.4fc38aac43fbbc756ecc7489e7c9041f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695b292740da2d7a9e5841fccf9b6995",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d15c9d28c8c87300dcf3",
+    "url": "/static/css/main.d46d62c7.chunk.css"
+  },
+  {
+    "revision": "991076cdaaba11ec9498",
+    "url": "/static/js/2.671d283b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.671d283b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d15c9d28c8c87300dcf3",
+    "url": "/static/js/main.3b0e9fa8.chunk.js"
+  },
+  {
+    "revision": "6465be2eacc601b1e906",
+    "url": "/static/js/runtime-main.3260c3d7.js"
+  }
+]);
